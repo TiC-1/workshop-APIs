988ea8bfc6247f92f7d74412942a96951a29528f
Matteo Giaccone <matteo@turnintocoders.it>
2018-02-19 18:25:52 +0100

    One way of testing with DOM and Ajax

--- public/index.js
+++ public/index.js
@@ -1,1 +1,23 @@
 /* let's go! */
+var updateGithubUser = function(cb) {
+  getGithubInfo(cb, updateDomGithubUser);
+}
+
+var getGithubInfo = function(cb, updateDom) {
+  var xhr = new XMLHttpRequest();
+  var url = "https://api.github.com/users/matjack1";
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        var githubObj = JSON.parse(xhr.responseText);
+        updateDom(cb, githubObj);
+      }
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+}
+
+var updateDomGithubUser = function(cb, obj){
+  var username = document.getElementById("github-user-handle");
+  username.textContent = obj.name;
+  cb();
+}
--- tests/tests.js
+++ tests/tests.js
@@ -1,31 +1,16 @@
-// just some sample functions, you can delete these
-// QUnit will find any functions you write in index.js too!
-function add(a, b) {
-  return a + b;
-}
+//
+// test("add function", function(assert) {
+//   var result = add(2, 3);
+//   var expected = 5;
+//   assert.equal(result, expected, "correctly adds two numbers");
+// });
 
-function addOne(arr) {
-  return arr.map(function(n) {
-    return n + 1;
+var username = document.getElementById("github-user-handle");
+
+function testUserName() {
+  return test("User name is Matteo Giaccone", function(assert) {
+    assert.equal(username.textContent, "Matteo Giaccone", "Sets user name");
   });
-}
+};
 
-// example tests - you can write your own tests like these :)
-// try playing about and watching them fail!
-test("add function", function(assert) {
-  var result = add(2, 3);
-  var expected = 5;
-  assert.equal(result, expected, "correctly adds two numbers");
-});
-
-test("addOne function", function(assert) {
-  var result = addOne([1, 2, 3]);
-  var expected = [2, 3, 4];
-  assert.deepEqual(
-    result,
-    expected,
-    "correctly adds one to each number in the array"
-  );
-});
-
-// add your own tests here using QUnit!
+updateGithubUser(testUserName);
